97eae6366e6695d55da9ac16fb4f9b4dfc7e678e
Nathan <nathan.perdoux@studenti.unitn.it>
2024-12-26 16:49:38 +0100

    Added ControllerShortcut + select_biased macro for the drone

--- src/drone.rs
+++ src/drone.rs
@@ -2,7 +2,7 @@
 //! Handles packet routing, flooding, and network management for drone nodes.
 
 use network_node::{log_status, Command, NetworkNode};
-use crossbeam_channel::{select, Receiver, Sender};
+use crossbeam_channel::{select, select_biased, Receiver, Sender};
 use rand::{rngs::StdRng, Rng, SeedableRng};
 use std::collections::{HashMap, HashSet};
 use wg_2024::{
@@ -29,44 +29,72 @@ impl NetworkNode for Drone {
     fn get_id(&self) -> NodeId {
         self.id
     }
-
+    
     fn get_seen_flood_ids(&mut self) -> &mut HashSet<u64> {
         &mut self.seen_flood_ids
     }
-
+    
     fn get_packet_send(&mut self) -> &mut HashMap<NodeId, Sender<Packet>> {
         &mut self.packet_send
     }
-
+    
     fn get_packet_receiver(&self) -> &Receiver<Packet> {
         &self.packet_recv
     }
-
+    
     fn get_random_generator(&mut self) -> &mut StdRng {
         &mut self.random_generator
     }
-
+    
     fn handle_routed_packet(&mut self, packet: Packet) {
         if !self.verify_routing(&packet) {
             return;
         }
-
+        
         // Handle final destination
         if packet.routing_header.hop_index + 1 == packet.routing_header.hops.len() {
-            let nack = self.build_nack(packet, NackType::DestinationIsDrone);
-            self.forward_packet(nack);
+            
+            if matches!(packet.pack_type, PacketType::MsgFragment(_)) {
+                let nack = self.build_nack(packet, NackType::DestinationIsDrone);
+                self.forward_packet(nack);
+            } else {
+                if let Err(e) = self
+                .sim_contr_send
+                .send(DroneEvent::ControllerShortcut(packet.clone()))
+                {
+                    log_status(
+                        self.id,
+                        &format!("Failed to send ControllerShortcut event: {:?}", e),
+                    );
+                }
+                self.forward_packet(packet);
+            }
+            
             return;
         }
-
+        
         let next_hop_id = packet.routing_header.hops[packet.routing_header.hop_index + 1];
-
+        
         // Check if next hop is reachable
         if !self.packet_send.contains_key(&next_hop_id) {
-            let nack = self.build_nack(packet, NackType::ErrorInRouting(next_hop_id));
-            self.forward_packet(nack);
+            if matches!(packet.pack_type, PacketType::MsgFragment(_)) {
+                let nack = self.build_nack(packet, NackType::ErrorInRouting(next_hop_id));
+                self.forward_packet(nack);
+            } else {
+                if let Err(e) = self
+                .sim_contr_send
+                .send(DroneEvent::ControllerShortcut(packet.clone()))
+                {
+                    log_status(
+                        self.id,
+                        &format!("Failed to send ControllerShortcut event: {:?}", e),
+                    );
+                }
+                self.forward_packet(packet);
+            }
             return;
         }
-
+        
         match packet.pack_type {
             PacketType::MsgFragment(_) => self.handle_message_fragment(packet),
             _ => {
@@ -76,11 +104,11 @@ impl NetworkNode for Drone {
                 //  if this gets implemented in the function, be sure to delete every place where the hop index is incremented before calling the function
                 let mut forward_packet = packet.clone();
                 forward_packet.routing_header.hop_index += 1;
-
+                
                 // Send PacketSent event for non-fragment packets too
                 if let Err(e) = self
-                    .sim_contr_send
-                    .send(DroneEvent::PacketSent(forward_packet.clone()))
+                .sim_contr_send
+                .send(DroneEvent::PacketSent(forward_packet.clone()))
                 {
                     log_status(
                         self.id,
@@ -91,7 +119,7 @@ impl NetworkNode for Drone {
             }
         }
     }
-
+    
     fn handle_command(&mut self, command: Command) {
         match command {
             Command::Drone(drone_command) => match drone_command {
@@ -103,7 +131,7 @@ impl NetworkNode for Drone {
             _ => panic!("Drone {} received a wrong command type", self.get_id()),
         }
     }
-
+    
 }
 
 impl wg_2024::drone::Drone for Drone {
@@ -127,11 +155,11 @@ impl wg_2024::drone::Drone for Drone {
             should_exit: false,
         }
     }
-
+    
     /// Main event loop for the drone.
     fn run(&mut self) {
         while !self.should_exit {
-            select! {
+            select_biased! {
                 recv(self.sim_contr_recv) -> command_res => {
                     if let Ok(drone_command) = command_res {
                         let command = Command::Drone(drone_command);
@@ -160,14 +188,14 @@ impl Drone {
         }
         true
     }
-
+    
     // Handling a message fragment for a drone is very different from the behaviour that a client or a server would have
     fn handle_message_fragment(&mut self, packet: Packet) {
         if self.should_drop_packet() {
             // Send event before modifying packet
             if let Err(e) = self
-                .sim_contr_send
-                .send(DroneEvent::PacketDropped(packet.clone()))
+            .sim_contr_send
+            .send(DroneEvent::PacketDropped(packet.clone()))
             {
                 log_status(
                     self.id,
@@ -178,14 +206,14 @@ impl Drone {
             self.forward_packet(nack);
             return;
         }
-
+        
         // Increment hop index for forwarding
         let mut forward_packet = packet.clone();
         forward_packet.routing_header.hop_index += 1;
-
+        
         if let Err(e) = self
-            .sim_contr_send
-            .send(DroneEvent::PacketSent(forward_packet.clone()))
+        .sim_contr_send
+        .send(DroneEvent::PacketSent(forward_packet.clone()))
         {
             log_status(
                 self.id,
@@ -194,23 +222,23 @@ impl Drone {
         }
         self.forward_packet(forward_packet);
     }
-
+    
     fn should_drop_packet(&mut self) -> bool {
         let pdr_scaled = (self.pdr * 100.0) as i32;
         self.get_random_generator().gen_range(0..=100) < pdr_scaled
     }
-
+    
     fn set_pdr(&mut self, new_pdr: f32) {
         self.pdr = new_pdr;
     }
-
+    
     fn crash(&mut self) {
         log_status(self.id, "Starting crash sequence");
-
+        
         while let Ok(packet) = self.packet_recv.try_recv() {
             self.handle_packet(packet, NodeType::Client);
         }
-
+        
         self.should_exit = true;
         log_status(self.id, "Crashed");
     }
@@ -219,16 +247,16 @@ impl Drone {
 #[cfg(test)]
 mod tests {
     use wg_2024::drone::Drone as _;
-
+    
     use super::*;
-
+    
     #[test]
     fn test_drone_creation() {
         let (controller_send, _) = crossbeam_channel::unbounded();
         let (_, controller_recv) = crossbeam_channel::unbounded();
         let (_, packet_recv) = crossbeam_channel::unbounded();
         let packet_send = HashMap::new();
-
+        
         let drone = Drone::new(
             1,
             controller_send,
@@ -237,7 +265,7 @@ mod tests {
             packet_send,
             0.0,
         );
-
+        
         assert_eq!(drone.id, 1);
         assert_eq!(drone.pdr, 0.0);
         assert!(drone.packet_send.is_empty());
